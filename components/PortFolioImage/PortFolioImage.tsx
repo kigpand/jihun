d07e9cb160ef7ba06d79fd00e9d5b82fe6add379
kigpand <kigpand@naver.com>
2022-12-25 19:43:19 +0900

    포폴 내용 추가

--- components/PortFolioImage/PortFolioImage.tsx
+++ components/PortFolioImage/PortFolioImage.tsx
@@ -11,12 +11,14 @@ const srcObj:any = {
     'poke1': '/imgs/pokemon/poke.png',
     'poke2': '/imgs/pokemon/poke2.png',
     'poke3': '/imgs/pokemon/poke3.png',
+    'poke4': '/imgs/pokemon/poke4.png',
     'jigram1': '/imgs/jigram/jigram.png',
     'jigram2': '/imgs/jigram/jigram2.png',
     'jigram3': '/imgs/jigram/jigram3.png',
     'shopping1': '/imgs/shopping/shopping.png',
     'shopping2': '/imgs/shopping/shopping2.png',
     'shopping3': '/imgs/shopping/shopping3.png',
+    'shopping4': '/imgs/shopping/shopping4.png',
     'youtube1': '/imgs/youtube/youtube.png',
     'youtube2': '/imgs/youtube/youtube2.png',
     'youtube3': '/imgs/youtube/youtube3.png',
@@ -77,6 +79,20 @@ const PortFolioImage = ({ title, src }: PortFolioImage) => {
                     }
                 });
             }
+            if (num === 4) {
+                if (title === 'poke') imgRef.current.src = srcObj['poke4'];
+                if (title === 'shopping') imgRef.current.src = srcObj['shopping4'];
+
+                btnRef.current?.childNodes.forEach((child: any, i: number) => {
+                    if (i === 3) {
+                        child.style.backgroundColor = 'white';
+                        child.style.transform = 'scale(1.3)';
+                    } else {
+                        child.style.backgroundColor = 'lightgray';
+                        child.style.transform = 'scale(1)';
+                    }
+                });
+            }
         }
     }
 
@@ -90,6 +106,7 @@ const PortFolioImage = ({ title, src }: PortFolioImage) => {
                 <div style={{ backgroundColor: 'white', transform: 'scale(1.3)'}} onClick={() => onBtnClick(1)}></div>
                 <div onClick={() => onBtnClick(2)}></div>
                 <div onClick={() => onBtnClick(3)}></div>
+                { (title === 'poke' || title === 'shopping') && <div onClick={() => onBtnClick(4)}></div>}
             </div>
         </div>
     )
